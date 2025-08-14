9867fb2421fba06a8b5f6a814d12cff4bc820b63
Arshad <sa0675878@gmail.com>
2025-08-14 13:07:46 +0530

    added missing gender variable

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -14,12 +14,14 @@ router = APIRouter(
 async def get_dashboard_data(user_db= Depends(get_authenticated_agent_db)):
     user, db = user_db  # Unpack user & database from function
     total_profiles = await db["user_profiles"].count_documents({})
-    
     # Count male users
     total_male = await db["user_profiles"].count_documents({"gender": "Male"})
 
     # Count female users
     total_female = await db["user_profiles"].count_documents({"gender": "Female"})
+    
+    #missing gender
+    missing_gender = total_profiles - ( total_female + total_male )
     print(user)
     print("jk;dj",db)
-    return JSONResponse(status_code=status.HTTP_200_OK, content={'total_profiles': total_profiles,'total_male': total_male, 'total_female': total_female})
+    return JSONResponse(status_code=status.HTTP_200_OK, content={'total_profiles': total_profiles,'total_male': total_male, 'total_female': total_female, "missing_gender": missing_gender})
